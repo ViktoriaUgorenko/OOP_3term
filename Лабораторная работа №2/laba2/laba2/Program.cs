faf5102f5093b16c0610df6557129f7044ba03c2
ViktoriaUgorenko <154615663+ViktoriaUgorenko@users.noreply.github.com>
2024-09-18 23:37:52 +0300

    Add files via upload

---
+++ Лабораторная работа №2/laba2/laba2/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Airline[] flights = new Airline[]
+        {
+            new Airline("Minsk", 10, "Airline-112", new DateTime(2024, 10, 18, 11, 45, 0), DayOfWeek.Monday),
+            new Airline("Astana", 11, "Airline-1876", new DateTime(2024, 12, 22, 3, 10, 0), DayOfWeek.Tuesday),
+            new Airline("Moscow", 12, "Airline-965", new DateTime(2024, 10, 17, 14, 0, 0), DayOfWeek.Wednesday),
+            new Airline("Warsaw", 13, "Airline-002", new DateTime(2024, 11, 29, 19, 50, 0), DayOfWeek.Wednesday),
+            new Airline("London", 14, "Airline-666", new DateTime(2024, 11, 5, 18, 25, 0), DayOfWeek.Friday),
+        };
+
+        foreach (var flight in flights)
+        {
+            flight.PrintFlightInfo();
+            Console.WriteLine();
+        }
+
+
+        Console.WriteLine("Исходный номер рейса: " + flights[0].FlightNumber);
+        int newFlightNumber;
+        do
+        {
+            Console.Write("Введите новый номер рейса: ");
+        } while (!int.TryParse(Console.ReadLine(), out newFlightNumber));
+        flights[0].UpdateFlightNumber(ref newFlightNumber);
+
+
+        int retrievedFlightNumber;
+        if (flights[0].TryGetFlightNumber(out retrievedFlightNumber))
+        {
+            Console.WriteLine("Новый номер рейса: " + retrievedFlightNumber);
+        }
+        else
+        {
+            Console.WriteLine("Не удалось получить номер рейса.");
+        }
+        flights[0].PrintFlightInfo();
+
+
+        string destinationToSearch = "London";
+        Console.WriteLine($"Список рейсов для пункта назначения '{destinationToSearch}':");
+        foreach (var flight in flights)
+        {
+            if (flight.Destination == destinationToSearch)
+            {
+                flight.PrintFlightInfo();
+                Console.WriteLine();
+            }
+        }
+
+
+        DayOfWeek dayOfWeekToSearch = DayOfWeek.Wednesday;
+        Console.WriteLine($"Список рейсов для дня недели '{dayOfWeekToSearch}':");
+        foreach (var flight in flights)
+        {
+            if (flight.DayOfWeek == dayOfWeekToSearch)
+            {
+                flight.PrintFlightInfo();
+                Console.WriteLine();
+            }
+        }
+
+
+        var flightInfo = new
+        {
+            Destination = "Praga",
+            FlightNumber = 099,
+            AircraftType = "Airline-444",
+            DepartureTime = new DateTime(2024, 11, 23, 14, 50, 0),
+            DayOfWeek = DayOfWeek.Monday
+        };
+
+        Console.WriteLine("Информация о рейсе анонимного типа:");
+        Console.WriteLine($"Пункт назначения: {flightInfo.Destination}");
+        Console.WriteLine($"Номер рейса: {flightInfo.FlightNumber}");
+        Console.WriteLine($"Тип самолета: {flightInfo.AircraftType}");
+        Console.WriteLine($"Время вылета: {flightInfo.DepartureTime}");
+        Console.WriteLine($"День недели: {flightInfo.DayOfWeek}");
+        Console.WriteLine("-----------------------------------------------------");
+
+        Airline defaultAirline = Airline.CreateDefaultAirline();
+        Console.WriteLine("Информация о рейсе по умолчанию:");
+        Console.WriteLine($"Пункт назначения: {defaultAirline.Destination}");
+        Console.WriteLine($"Номер рейса: {defaultAirline.FlightNumber}");
+        Console.WriteLine($"Тип самолета: {defaultAirline.AircraftType}");
+        Console.WriteLine($"Время вылета: {defaultAirline.DepartureTime}");
+        Console.WriteLine($"День недели: {defaultAirline.DayOfWeek}");
+        Console.WriteLine("-----------------------------------------------------");
+
+        Console.WriteLine("Equals номера рейса третьего рейса с первым рейсом: " + flights[2].Equals(flights[0]));
+        Console.WriteLine("ToString: " + flights[2].ToString());
+
+        Airline.PrintClassInfo();
+
+    }
+}
---
+++ Лабораторная работа №2/laba2/laba2/airline.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+public partial class Airline
+{
+    private string destination;
+    private int flightNumber;
+    private string aircraftType;
+    private DateTime departureTime;
+    private DayOfWeek dayOfWeek;
+
+    private readonly int id;
+
+    private const string airlineName = "MyAirline";
+    
+    private static int numberOfFlights;
+
+    /*конструкторы*/
+    public Airline(string destination, int flightNumber, string aircraftType, DateTime departureTime, DayOfWeek dayOfWeek)
+    {
+        this.destination = destination;
+        this.flightNumber = flightNumber;
+        this.aircraftType = aircraftType;
+        this.departureTime = departureTime;
+        this.dayOfWeek = dayOfWeek;
+        this.id = this.GetHashCode();
+        numberOfFlights++;
+    }
+
+    public Airline(string destination, int flightNumber, string aircraftType, DateTime departureTime)
+        : this(destination, flightNumber, aircraftType, departureTime, DayOfWeek.Monday)
+    {
+    }
+
+   
+    static Airline()
+    {
+        numberOfFlights = 0;
+        Console.WriteLine("Статический конструктор класса Airline вызван.\n\n");
+    }
+
+    private Airline()
+    {
+        destination = "Berlin";
+        flightNumber = 19;
+        aircraftType = "Airline-678";
+        departureTime = new DateTime(2024, 12, 24, 8, 30, 0);
+        dayOfWeek = DayOfWeek.Monday;
+        numberOfFlights++;
+    }
+    /*вариант его вызова*/
+    public static Airline CreateDefaultAirline()
+    {
+        return new Airline();
+    }
+
+    public void UpdateFlightNumber(ref int newFlightNumber)
+    {
+        flightNumber = newFlightNumber;
+    }
+
+    public bool TryGetFlightNumber(out int flightNumber)
+    {
+        flightNumber = FlightNumber;
+        return true;
+    }
+
+    public static void PrintClassInfo()
+    {
+        Console.WriteLine($"Класс Airline. Количество созданных объектов: {numberOfFlights}");
+    }
+
+    public void PrintFlightInfo()
+    {
+        Console.WriteLine($"-----------------------{airlineName}---------------------");
+        Console.WriteLine($"Пункт назначения: {Destination}");
+        Console.WriteLine($"Номер рейса: {FlightNumber}");
+        Console.WriteLine($"Тип самолета: {AircraftType}");
+        Console.WriteLine($"Время вылета: {DepartureTime}");
+        Console.WriteLine($"День недели: {DayOfWeek}");
+        Console.WriteLine($"ID: {ID}");
+        Console.WriteLine("-----------------------------------------------------");
+    }
+
+    /*переопределения*/
+    public override string ToString()
+    {
+        return $"Рейс {FlightNumber} - {Destination}";
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+            return false;
+
+        Airline other = (Airline)obj;
+        return this.FlightNumber == other.FlightNumber;
+    }
+
+    public override int GetHashCode()
+    {
+        return FlightNumber.GetHashCode();
+    }
+}
